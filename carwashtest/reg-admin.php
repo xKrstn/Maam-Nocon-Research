905c42d4a2c1e3987475bdf60689017197404ac5
Adrian Jensen <147803474+damoojiii@users.noreply.github.com>
2024-06-27 08:46:11 +0800

    register ni admin

---
+++ carwashtest/reg-admin.php
@@ -0,0 +1,77 @@
+<?php 
+    include '../include/connection.php';
+
+    if(isset($_POST['submit'])){
+        $firstname=filter_input(INPUT_POST,'firstname',FILTER_SANITIZE_SPECIAL_CHARS);
+        $lastname=filter_input(INPUT_POST,'lastname',FILTER_SANITIZE_SPECIAL_CHARS);
+        $username=filter_input(INPUT_POST,'username',FILTER_SANITIZE_SPECIAL_CHARS);
+        $password=filter_input(INPUT_POST,'password',FILTER_SANITIZE_SPECIAL_CHARS);
+        $conpass=filter_input(INPUT_POST,'conpass',FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if($password !== $conpass){
+            echo "<script> alert('Password and Confirm Password Do Not Match');</script>";
+            echo '<script> window.location.href="registration/reg.html";</script>';
+        }
+        else{
+            $sql = "SELECT * FROM manager_tbl";
+            $result = $con->query($sql);
+
+            if($result->num_rows == 1){
+                echo "<script> alert('Maximum Limit Has Been Reached'); </script>";
+                echo '<script> window.location.href="reg-admin.php";</script>'; 
+            }
+            else{
+                $hashpass = password_hash($password, PASSWORD_DEFAULT);
+                $sql = "INSERT INTO manager_tbl(firstname, lastname, username,password) VALUES ('$firstname','$lastname','$username','$hashpass')";
+
+                if($con->query($sql)===TRUE){
+                    echo '<script> alert("Registered Successfully");</script>';
+                    echo '<script> window.location.href="../registration/login.html";</script>'; 
+                }
+                else{
+                    echo '<script>alert("Error: ' .$sql. '\n'. $con->error.'");</script>';
+                    echo '<script> window.location.href="reg-admin.php";</script>';     
+                }
+            }
+        }   
+    }
+
+$con->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carwash</title>
+    <link rel="stylesheet" href="../registration/login-reg.css">
+</head>
+<body>
+    
+    <div class="box2">
+        <h2>Register</h2>
+        <form method="POST" class="formbox">
+            <div class="column">
+                <div class="input">
+                    <input type="text" name="firstname" placeholder="First Name" required>
+                </div>
+                <div class="input">
+                    <input type="text" name="lastname" placeholder="Last Name" required>
+                </div>
+            </div>
+            <div class="input">
+                <input type="text" name="username" placeholder="Username" required>
+            </div>
+            <div class="input">
+                <input type="password" name="password" placeholder="Password" required>
+            </div>
+            <div class="input">
+                <input type="password" name="conpass" placeholder="Confirm Password" required>
+            </div>
+            <button type="submit" name="submit" class="button">Register</button>
+        </form>
+    </div>
+    
+</body>
+</html>
